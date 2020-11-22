cddd1216d0f60faec189fd44880c4dfa0ae64917
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-22 23:00:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -40,9 +40,9 @@ end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
-  # remember to return the updated hash
-  holiday_hash[season] << holiday_name
-
+  # remember to return the updated hashs
+  binding.pry
+  holiday_hash[season]
 end
 
 def all_winter_holiday_supplies(holiday_hash)
