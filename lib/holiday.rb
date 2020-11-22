ef6c2d8e5177cd01f2f3050a629d4a358ad82573
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-22 23:52:11 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season|
-    puts season.capitalize + ":"
+    puts season.to_s.capitalize + ":"
     season.each do |holiday|
       puts holiday.capitalize
     end
