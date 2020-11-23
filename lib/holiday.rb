616c942f42a6fd5148ae72c27c3483df4f626f54
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-23 04:48:34 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays|
     puts season.to_s.capitalize + ":"
     holidays.each do |holiday, supplies|
-      puts holiday.split.map(&:capitalize).join(' ') + ": " supplies
+      puts holiday.split.map(&:capitalize).join(' ') + ": " + supplies
     end
 
   end
