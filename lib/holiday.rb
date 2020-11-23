4ac1f943fb2d45f3306d0d94b6762000000d565c
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-23 05:22:12 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,7 +83,7 @@ end
 
 def format_holiday(holiday)
   #convert holiday into titlecase
-    holiday.to_s.split.each{|i| i.capitalize!}.join(' ')
+    holiday.to_s.split.map { |x| x.capitalize }.join(" ")
   end
 
 
