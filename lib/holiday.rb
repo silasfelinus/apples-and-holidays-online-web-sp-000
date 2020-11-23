20780da452efe5017ff7c9f57fc55228a3b48447
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-23 09:21:48 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -84,12 +84,31 @@ end
 
 
 
+#   :winter => {
+#     :christmas => ["Lights", "Wreath"],
+#     :new_years => ["Party Hats"]
+#   },
+#   :summer => {
+#     :fourth_of_july => ["Fireworks", "BBQ"]
+#   },
+#   :fall => {
+#     :thanksgiving => ["Turkey"]
+#   },
+#   :spring => {
+#     :memorial_day => ["BBQ"]
+
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
-  working_array = holiday_hash.values.collect do |season|
-    season
+  bbq_holidays = []
+  holiday_hash.each do |season|
+    season.each do |holiday|
+      holiday.each do |supplies|
+      if supplies == "BBQ"
+        bbq_holidays << holiday
+      end
+    end
   end
-  working_array
+  bbq_holidays
+  end
 end
