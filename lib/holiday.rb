36c229a88c02338c3863eb2d3eab5cb2fd4193f8
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-23 09:08:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,5 +87,5 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  holiday_hash.values
+  holiday_hash.values.length
 end
