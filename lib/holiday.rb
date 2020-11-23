a31ba095324e2cbae062852f8cefb7c9285806d3
Silas Michael Knight <silasfelinus@gmail.com>
2020-11-23 08:37:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,7 +87,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  bbq_holidays = holiday_hash.values.collect do |holiday, supplies|
+  bbq_holidays = holiday_hash.values.collect do |holiday|
     holiday
   end
   binding.pry
